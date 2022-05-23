aad128b0ee07130add2a2c49bf7efb790b20c825
saleem <saleem.coder@gmail.com>
2022-05-23 17:24:16 +0500

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalogue/pages/home_page.dart';
 import 'package:flutter_catalogue/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: HomePage(),
 
-      // themeMode: ThemeMode.light,
-      // theme: ThemeData(primarySwatch: Colors.deepOrange),
-      // darkTheme: ThemeData(
-      //   brightness: Brightness.dark,
-      // ),
-      initialRoute: "/home",
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      //initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,13 +4,72 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              color: Colors.orange, fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+        color: Color.fromRGBO(199, 216, 247, 1),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80.0,
+            ),
+            Image.asset(
+              "assets/images/saleem.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "Login to Continue",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Padding(
+              //padding: const EdgeInsets.all(30.0),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("TechWare House");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+
+    // return Material(
+    //   child: Center(
+    //     child: Text(
+    //       "Login Page",
+    //       style: TextStyle(
+    //           color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold),
+    //     ),
+    //   ),
+    // );
   }
 }
